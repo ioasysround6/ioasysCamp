849797ed71ddf7c15be9275fb8578d2123d0abe3
Pedro Henrique <pedroabreu.dev@gmail.com>
2022-03-28 15:47:17 -0400

    feat: Criando o gráfico de Navegação, um fragmento e refatorando o projeto.

---
+++ Android/round6/app/src/main/java/br/com/ioasys/round6/activity/MainActivity.kt
@@ -0,0 +1,22 @@
+package br.com.ioasys.round6.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.ioasys.round6.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
