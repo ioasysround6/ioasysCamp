272c4b66a9796c2fd0e7f56b504005b0acb10f61
Vinicius Canaan <60820733+ViniciusCanaan@users.noreply.github.com>
2022-03-29 17:46:00 -0300

    Adicionando restante de cores na paleta

--- React Native/round6/src/styles/colors.js
+++ React Native/round6/src/styles/colors.js
@@ -1,10 +1,23 @@
 export const colors = {
-    primaryDefault: '#F86624',
+
     primaryDark: '#E15D21',
+    primaryDefault: '#F86624',
+    primaryLight: '#FA844E',
 
-    secondaryDefault: '#EA3546',
     secondaryDarker: '#A41623',
+    secondaryDark: '#C72635',
+    secondaryDefault: '#EA3546',
+    secondaryLight: '#EE5866',
 
+    neutralDarker: '#252A27',
+    neutralDark: '#545956',
+    neutralMediumDark: '#ACAEAD',
+    neutralMediumLight: '#D1D2D1',
+    neutralLight: '#F5F5F5',
     neutralLighter: '#FCFCFC',
+
+    indicativeAlert: '#F9C80E',
+    indicativeSuccess: '#169873',
+    indicativeError: '#F72C25',
 
 }
