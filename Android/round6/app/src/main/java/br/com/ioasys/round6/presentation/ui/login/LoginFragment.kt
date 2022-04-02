7692d4e7d10cbd10700241be88ffa21705ccbfe3
saracavalcante <designersaracavalcante@gmail.com>
2022-04-02 11:07:58 -0300

    feat: Adiciona tela de login e altera ordem de navegação

--- Android/round6/app/src/main/java/br/com/ioasys/round6/presentation/ui/login/LoginFragment.kt
+++ Android/round6/app/src/main/java/br/com/ioasys/round6/presentation/ui/login/LoginFragment.kt
@@ -1,13 +1,13 @@
 package br.com.ioasys.round6.presentation.ui.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import br.com.ioasys.round6.R
 import br.com.ioasys.round6.databinding.FragmentLoginBinding
-import br.com.ioasys.round6.databinding.FragmentTripsBinding
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
@@ -24,9 +24,19 @@ class LoginFragment : Fragment() {
         _binding = this
     }.root
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener() {
+        binding.btnSingIn.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_nav_graph)
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
