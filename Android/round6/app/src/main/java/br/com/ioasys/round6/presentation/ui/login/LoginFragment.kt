e83e9bdd615bc2512bc3a597541bbb0f46686650
saracavalcante <designersaracavalcante@gmail.com>
2022-04-03 11:37:03 -0300

    feat: Adiciona visibilidade de senha

--- Android/round6/app/src/main/java/br/com/ioasys/round6/presentation/ui/login/LoginFragment.kt
+++ Android/round6/app/src/main/java/br/com/ioasys/round6/presentation/ui/login/LoginFragment.kt
@@ -1,6 +1,8 @@
 package br.com.ioasys.round6.presentation.ui.login
 
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +14,8 @@ import br.com.ioasys.round6.databinding.FragmentLoginBinding
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding: FragmentLoginBinding get() = _binding!!
+
+    private var isShowPass = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +31,36 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setOnClickListener()
+        showPassword(isShowPass)
     }
 
     private fun setOnClickListener() {
-        binding.btnSingIn.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_nav_graph)
+        binding.apply {
+            btnSingIn.setOnClickListener {
+                findNavController().navigate(R.id.action_loginFragment_to_nav_graph)
+            }
+
+            passwordToggle.setOnClickListener {
+                isShowPass = !isShowPass
+                showPassword(isShowPass)
+            }
         }
+    }
+
+    private fun showPassword(isShow: Boolean) {
+        if (isShow) {
+            binding.apply {
+                inputPassword.transformationMethod =
+                    HideReturnsTransformationMethod.getInstance()
+                passwordToggle.setImageResource(R.drawable.hide_password)
+            }
+        } else {
+            binding.apply {
+                inputPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+                passwordToggle.setImageResource(R.drawable.show_password)
+            }
+        }
+        binding.inputPassword.setSelection(binding.inputPassword.text.toString().length)
     }
 
     override fun onDestroyView() {
