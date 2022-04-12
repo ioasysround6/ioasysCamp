1cc2ed0a91bf38336e5a92eeb8e4de52f2118785
saracavalcante <designersaracavalcante@gmail.com>
2022-04-12 17:27:44 -0300

    feat: Altera textos da tela de confirmação de pagamento

--- Android/round6/app/src/main/java/br/com/ioasys/round6/presentation/ui/about/AboutFragment.kt
+++ Android/round6/app/src/main/java/br/com/ioasys/round6/presentation/ui/about/AboutFragment.kt
@@ -1,12 +1,9 @@
 package br.com.ioasys.round6.presentation.ui.about
 
-import android.graphics.Outline
 import android.os.Bundle
-import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewOutlineProvider
 import androidx.fragment.app.Fragment
 import br.com.ioasys.round6.databinding.FragmentAboutBinding
 
@@ -21,44 +18,8 @@ class AboutFragment : Fragment() {
         _binding = this
     }.root
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setCorners()
-    }
-
-    private fun setCorners() {
-        val cardView = binding.ivCardView
-        val image = binding.aboutImage
-
-        val outlineProvider = object : ViewOutlineProvider() {
-            override fun getOutline(view: View?, outline: Outline?) {
-                val left = 0
-                val top = 0
-                val right = view?.width
-                val bottom = view?.height
-                val cornerRadiusDP = 30f
-                val cornerRadius = TypedValue.applyDimension(
-                    TypedValue.COMPLEX_UNIT_DIP,
-                    cornerRadiusDP,
-                    resources.displayMetrics
-                ).toInt()
-
-                if (right != null && bottom != null) {
-                    outline?.setRoundRect(
-                        left,
-                        top - cornerRadius,
-                        right,
-                        bottom,
-                        cornerRadius.toFloat()
-                    )
-                }
-            }
-        }
-
-        image.outlineProvider = outlineProvider
-        image.clipToOutline = true
-
-        cardView.outlineProvider = outlineProvider
-        cardView.clipToOutline = true
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
