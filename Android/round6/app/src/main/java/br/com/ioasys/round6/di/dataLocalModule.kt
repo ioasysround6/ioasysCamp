fa7881891d8b6a97aed3bd65a3eab18dd294332d
saracavalcante <designersaracavalcante@gmail.com>
2022-04-16 17:05:03 -0300

    feat: Configura estrutura para salvar dados no shared preferences

---
+++ Android/round6/app/src/main/java/br/com/ioasys/round6/di/dataLocalModule.kt
@@ -0,0 +1,13 @@
+package br.com.ioasys.round6.di
+
+import br.com.ioasys.round6.data.data_source.local.LoginLocalDataSource
+import br.com.ioasys.round6.data_local.data_source.LoginLocalDataSourceImpl
+import br.com.ioasys.round6.data_local.utils.SharedPreferencesHelper
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+val dataLocalModule = module {
+
+    single { SharedPreferencesHelper(androidContext()) }
+    single<LoginLocalDataSource> { LoginLocalDataSourceImpl(get()) }
+}
