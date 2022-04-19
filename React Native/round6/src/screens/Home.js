cf71026f46cca4a136c9ab10e028f65e39050ef9
Rodrigo Mendes <rdrmendes@hotmail.com>
2022-04-18 21:12:09 -0300

    feat: retirando a navegacao na home

--- React Native/round6/src/screens/Home.js
+++ React Native/round6/src/screens/Home.js
@@ -55,10 +55,6 @@ export function Home() {
     buscaComunidades();
   }, [])
 
-  function handlePacote(item){
-		navigation.navigate("TravelPackageScreen", { item });
-	}
-
   return (
     <ScrollView>
       <ScreenView>
@@ -75,7 +71,7 @@ export function Home() {
             showsHorizontalScrollIndicator={false}
             renderItem={({ item }) => (
               <View style={{ flexDirection: 'row' }}>
-                <CardPackages onPress={() => handlePacote(item)} title={item.tourName} subtitle={item.communityName} altura={200} imgBack={item.photo1} />
+                <CardPackages title={item.tourName} subtitle={item.communityName} altura={200} imgBack={item.photo1} />
               </View>
 
             )}
