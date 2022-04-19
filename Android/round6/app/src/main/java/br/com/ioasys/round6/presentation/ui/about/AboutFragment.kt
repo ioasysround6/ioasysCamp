1bbfd0508a878c75d279d3c5c8fcc5612f3cdb3e
saracavalcante <designersaracavalcante@gmail.com>
2022-04-19 14:38:24 -0300

    feat: Adiciona carrossel de imagens na tela de home

--- Android/round6/app/src/main/java/br/com/ioasys/round6/presentation/ui/about/AboutFragment.kt
+++ Android/round6/app/src/main/java/br/com/ioasys/round6/presentation/ui/about/AboutFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import br.com.ioasys.round6.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
@@ -17,6 +18,17 @@ class AboutFragment : Fragment() {
     ): View = FragmentAboutBinding.inflate(inflater, container, false).apply {
         _binding = this
     }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener() {
+        binding.btnBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
